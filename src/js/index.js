0271d65d43d87a8c000148f0409557e4121cdda8
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2024-11-23 21:39:09 -0400

    Upgrade

--- src/js/index.js
+++ src/js/index.js
@@ -2,27 +2,6 @@ const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 let topBtn = document.getElementById("topBtn");
 
-// Quando o usuário clicar no botão irá ativar as funções
-
-botaoMostrarProjetos.addEventListener('click', () => {
-    mostrarMaisProjetos();
-    esconderBotao();
-});
-
-// Depois de clicar no botão ele fica oculto
-
-function esconderBotao() {
-    botaoMostrarProjetos.classList.add("remover");
-}
-
-// Mostra os projetos que estavam ocultos
-
-function mostrarMaisProjetos() {
-    projetosInativos.forEach(projetosInativos => {
-        projetosInativos.classList.add('ativo');
-    });
-}
-
 // Quando o usuário rolar para baixo mostre o botão
 
 window.onscroll = function () {scrollFunction()};
